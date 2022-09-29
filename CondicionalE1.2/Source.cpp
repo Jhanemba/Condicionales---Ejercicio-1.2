4dc81bdc12026566361c6e0e1b1a657791ecf814
Kevin Eduardo Cardenas mamani <kevin.kc541@gmail.com>
2022-09-29 14:36:07 -0400

    Se resume la entrada, comprimiendo 2 entradas en una sola línea.

--- CondicionalE1.2/Source.cpp
+++ CondicionalE1.2/Source.cpp
@@ -4,4 +4,20 @@ Escriba un programa que lea dos numeros y determine cual de ellos es el mayor
 #include<iostream>
 using namespace std;
 int main()
-{}
+{
+	int a, b;
+
+	cout << "Introduzca 2 numeros: " << endl;
+	cin >> a >> b;
+
+	if(a > b)
+	{
+		cout << "El numero mayor es " << a;
+	}
+	else
+	{
+		cout << "El numero mayor es " << b;
+	}
+
+	return 0;
+}
